51c84edc9bb21564c37f5ac8952eabb2e1b08dc6
Wjoe21 <WasifJ21@hotmail.com>
2021-02-12 19:24:55 -0500

    testing routing

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-const infoFile = fs.readFileSync(path.join(__dirname, "data/info.json"));
+const infoFile = fs.readFileSync("./data/info.json");
 const parsedData = JSON.parse(infoFile);
 
 app.use(express.static("public"));
@@ -21,7 +21,7 @@ app.post("/dryfood", (req, res) => {
     let clickTime = date.format(new Date(), "ddd, MMM DD YYYY at hh:mm A");
     parsedData.DryFood = clickTime;
     let editedData = JSON.stringify(parsedData, null, 2);
-    fs.writeFileSync("./Data/info.json", editedData);
+    fs.writeFileSync("./data/info.json", editedData);
     res.sendStatus(201);
 });
 
@@ -29,7 +29,7 @@ app.post("/wetfood", (req, res) => {
     let clickTime = date.format(new Date(), "ddd, MMM DD YYYY at hh:mm A");
     parsedData.WetFood = clickTime;
     let editedData = JSON.stringify(parsedData, null, 2);
-    fs.writeFileSync("./Data/info.json", editedData);
+    fs.writeFileSync("./data/info.json", editedData);
     res.sendStatus(201);
 });
 
